ef256d741f3b43e03c9b4dbd650ec0a4b2981c0d
Joel Camargo <joelc1225@gmail.com>
2018-06-27 23:49:49 -0400

    Add overflow menu and add drawer layout to activityMain.xml
    Still trying to get the hamburger icon and drawerlayout working right

--- app/src/main/java/com/joelcamargojr/androidhub/MainActivity.java
+++ app/src/main/java/com/joelcamargojr/androidhub/MainActivity.java
@@ -1,14 +1,25 @@
 package com.joelcamargojr.androidhub;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import com.joelcamargojr.androidhub.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
     }
 }
