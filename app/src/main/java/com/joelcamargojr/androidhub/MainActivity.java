3339a0d259090e2431e9af1fd6f8043e145a9611
Joel Camargo <joelc1225@gmail.com>
2018-06-29 13:51:55 -0400

    Add the toolbar to main XML and nav_menu button (hamburger icon)
    changed the theme to NOACTIONBAR so i can implement my own with the nav menu button.
    i set it up in mainactivity per android documentation

--- app/src/main/java/com/joelcamargojr/androidhub/MainActivity.java
+++ app/src/main/java/com/joelcamargojr/androidhub/MainActivity.java
@@ -2,7 +2,9 @@ package com.joelcamargojr.androidhub;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 import com.joelcamargojr.androidhub.databinding.ActivityMainBinding;
@@ -15,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.nav_menu_icon);
     }
 
     @Override
