fa42e27c1ad6f606581a75a7ad3c7dcd91b04572
Joel Camargo <joelc1225@gmail.com>
2018-07-02 19:37:42 -0400

    Set up the nav drawer to open/close on nav menu button click

--- app/src/main/java/com/joelcamargojr/androidhub/MainActivity.java
+++ app/src/main/java/com/joelcamargojr/androidhub/MainActivity.java
@@ -2,31 +2,62 @@ package com.joelcamargojr.androidhub;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.joelcamargojr.androidhub.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding =
+        // create data binding instance for views
+        final ActivityMainBinding binding =
                 DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        // set up custom toolbar
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.nav_menu_icon);
+
+        mDrawerLayout = binding.drawerLayout;
+
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                binding.drawerLayout.closeDrawers();
+
+                // TODO switch statement for item selections and logic
+                return true;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.settings_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
