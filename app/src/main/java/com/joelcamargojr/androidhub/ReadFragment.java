6f239fca5efdbf0e7691fc1276760c374fb3c498
Joel Camargo <joelc1225@gmail.com>
2018-07-02 23:55:32 -0400

    Create the three fragment classes for the three tabs

---
+++ app/src/main/java/com/joelcamargojr/androidhub/ReadFragment.java
@@ -0,0 +1,19 @@
+package com.joelcamargojr.androidhub;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReadFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab_fragment_layout, container, false);
+        return view;
+    }
+}
