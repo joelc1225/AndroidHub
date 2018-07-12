785b9976a8fb8d614ac93806dbadbc868fc3b2b6
Joel Camargo <joelc1225@gmail.com>
2018-07-12 00:11:38 -0400

    Add Parceler library/removed pointless redundant adapter class

--- app/src/main/java/com/joelcamargojr/androidhub/model/Article.java
+++ app/src/main/java/com/joelcamargojr/androidhub/model/Article.java
@@ -1,13 +1,20 @@
 package com.joelcamargojr.androidhub.model;
 
+import org.parceler.Parcel;
+
+@Parcel
 public class Article {
 
-    private String mTitle;
-    private String mImageUrl;
+    String mTitle;
+    String mImageUrl;
 
     public Article (String title, String imageUrl) {
         this.mTitle = title;
         this.mImageUrl = imageUrl;
+    }
+
+    // Empty constructor needed by Parceler library
+    public Article() {
     }
 
     public String getTitle() {
