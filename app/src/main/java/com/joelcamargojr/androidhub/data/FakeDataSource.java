ebd1790804c636c30fa4a453658a9f9c8ab1b984
Joel Camargo <joelc1225@gmail.com>
2018-07-25 13:50:39 -0400

    Adjust fakeDataSource class to include new arrayLists for each ViewPager tab

--- app/src/main/java/com/joelcamargojr/androidhub/data/FakeDataSource.java
+++ app/src/main/java/com/joelcamargojr/androidhub/data/FakeDataSource.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 public class FakeDataSource {
 
     public static ArrayList<Article> articleList = new ArrayList<>();
+    public static ArrayList<Article> articleList2 = new ArrayList<>();
+    public static ArrayList<Article> articleList3 = new ArrayList<>();
 
     public static ArrayList<Article> getFakeArticleList() {
         articleList.add(new Article("Title 1", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
@@ -23,30 +25,30 @@ public class FakeDataSource {
     }
 
     public static ArrayList<Article> getFakeArticleList2() {
-        articleList.add(new Article("Title 1", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 2", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 3", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 4", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 5", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 6", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 7", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 8", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 9", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 10", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        return articleList;
+        articleList2.add(new Article("Title 1", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 2", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 3", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 4", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 5", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 6", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 7", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 8", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 9", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList2.add(new Article("Title 10", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        return articleList2;
     }
 
     public static ArrayList<Article> getFakeArticleList3() {
-        articleList.add(new Article("Title 1", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 2", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 3", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 4", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 5", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 6", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 7", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 8", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 9", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        articleList.add(new Article("Title 10", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
-        return articleList;
+        articleList3.add(new Article("Title 1", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 2", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 3", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 4", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 5", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 6", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 7", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 8", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 9", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        articleList3.add(new Article("Title 10", "https://media.pitchfork.com/photos/5aabd03c7e80eb754031ef32/2:1/w_790/Run-the-Jewels-Rick-and-Morty.jpg"));
+        return articleList3;
     }
 }
