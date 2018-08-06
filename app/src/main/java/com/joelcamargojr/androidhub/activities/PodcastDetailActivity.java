3fa5eaab619f22607002204f5540e8542df9262b
Joel Camargo <joelc1225@gmail.com>
2018-08-06 14:03:00 -0400

    Implement the new PodcastDetailActivity and EpisodeRV

--- app/src/main/java/com/joelcamargojr/androidhub/activities/PodcastDetailActivity.java
+++ app/src/main/java/com/joelcamargojr/androidhub/activities/PodcastDetailActivity.java
@@ -1,13 +1,49 @@
 package com.joelcamargojr.androidhub.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class Podcast_Detail extends AppCompatActivity {
+import com.joelcamargojr.androidhub.R;
+import com.joelcamargojr.androidhub.databinding.PodcastDetailBinding;
+import com.joelcamargojr.androidhub.model.Podcast;
+import com.joelcamargojr.androidhub.recyclerview.EpisodeRecyclerviewAdapter;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import timber.log.Timber;
+
+public class PodcastDetailActivity extends AppCompatActivity {
+
+    PodcastDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_podcast__detail);
+        binding = DataBindingUtil.setContentView(this, R.layout.podcast_detail);
+
+        // Gets bundle that should have our Podcast data
+        Bundle bundle = getIntent().getBundleExtra("bundle");
+
+        if (bundle != null) {
+            Timber.d("BUNDLE NOT NULL!");
+            Podcast podcast = Parcels.unwrap(bundle.getParcelable("podcast"));
+            Timber.d(podcast.image);
+            Picasso.get()
+                    .load(R.drawable.fragmented_image)
+                    .into(binding.podcastImageview);
+            binding.podcastDescriptionTextview.setText(podcast.description);
+            binding.podcastTitleTextView.setText(podcast.title);
+            RecyclerView episodesRV = binding.episodesRV;
+            EpisodeRecyclerviewAdapter adapter = new EpisodeRecyclerviewAdapter(this, podcast.episodeArrayList);
+            episodesRV.setAdapter(adapter);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+            episodesRV.setLayoutManager(layoutManager);
+
+        }
+
     }
 }
--- app/src/main/java/com/joelcamargojr/androidhub/recyclerview/EpisodeRecyclerviewAdapter.java
+++ app/src/main/java/com/joelcamargojr/androidhub/recyclerview/EpisodeRecyclerviewAdapter.java
@@ -1,4 +1,81 @@
 package com.joelcamargojr.androidhub.recyclerview;
 
-public class EpisodeRecyclerviewAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.joelcamargojr.androidhub.R;
+import com.joelcamargojr.androidhub.Utils.DateUtils;
+import com.joelcamargojr.androidhub.Utils.TimeUtils;
+import com.joelcamargojr.androidhub.model.Episode;
+
+import java.util.ArrayList;
+
+import static com.joelcamargojr.androidhub.Utils.StringUtils.stripHtml;
+
+public class EpisodeRecyclerviewAdapter extends RecyclerView.Adapter<EpisodeRecyclerviewAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Episode> episodes;
+
+    public EpisodeRecyclerviewAdapter(Context context, ArrayList<Episode> episodes) {
+        this.context = context;
+        this.episodes = episodes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.episode_list_item, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Gets current Episode item
+        Episode currentEpisode = episodes.get(position);
+
+        // gets the date and audio length to convert before populating to views
+        long dateLong = currentEpisode.date;
+        int audioLength = currentEpisode.audio_length;
+        String dateString = DateUtils.convertLongDateToString(dateLong);
+        String audioLengthString = TimeUtils.secondsToMinutes(audioLength);
+
+        holder.titleTV.setText(currentEpisode.title);
+        holder.descriptionTV.setText(stripHtml(currentEpisode.description));
+        holder.dateTV.setText(dateString);
+        holder.lengthTV.setText(audioLengthString);
+        holder.progressBar.setProgress(20);
+    }
+
+    @Override
+    public int getItemCount() {
+        return episodes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTV;
+        TextView descriptionTV;
+        TextView dateTV;
+        TextView lengthTV;
+        ProgressBar progressBar;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            titleTV = itemView.findViewById(R.id.episode_title_TV);
+            descriptionTV = itemView.findViewById(R.id.episode_descrip_TV);
+            dateTV = itemView.findViewById(R.id.episode_date_TV);
+            lengthTV = itemView.findViewById(R.id.episode_length_TV);
+            progressBar = itemView.findViewById(R.id.episode_progressBar);
+
+        }
+    }
+
 }
