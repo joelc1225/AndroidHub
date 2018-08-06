42459d6bb780682ad2fcf8f30885d6038b7e6de9
Joel Camargo <joelc1225@gmail.com>
2018-08-06 14:00:38 -0400

    Refactor Utils classes to add comments

--- app/src/main/java/com/joelcamargojr/androidhub/Utils/StringUtils.java
+++ app/src/main/java/com/joelcamargojr/androidhub/Utils/StringUtils.java
@@ -1,4 +1,11 @@
 package com.joelcamargojr.androidhub.Utils;
 
+import android.text.Html;
+
 public class StringUtils {
+
+    // Strips HTML tags from Strings received from API
+    public static String stripHtml(String htmlString) {
+        return Html.fromHtml(htmlString).toString();
+    }
 }
--- app/src/main/java/com/joelcamargojr/androidhub/Utils/TimeUtils.java
+++ app/src/main/java/com/joelcamargojr/androidhub/Utils/TimeUtils.java
@@ -1,4 +1,11 @@
 package com.joelcamargojr.androidhub.Utils;
 
+import java.util.concurrent.TimeUnit;
+
 public class TimeUtils {
+
+    // Converts the length of the audio file from int to String that we get from API
+    public static String secondsToMinutes(int seconds) {
+        return String.valueOf(TimeUnit.SECONDS.toMinutes(seconds));
+    }
 }
