ae1b01dde588fc662c8eb6d4d585827267d8541d
Joel Camargo <joelc1225@gmail.com>
2018-08-20 18:40:21 -0400

    Add listenNotes episode url as object paramter

--- app/src/main/java/com/joelcamargojr/androidhub/model/Episode.java
+++ app/src/main/java/com/joelcamargojr/androidhub/model/Episode.java
@@ -19,18 +19,21 @@ public class Episode {
     public String description;
     @SerializedName("pub_date_ms")
     public long date;
+    @SerializedName("listennotes_url")
+    public String listennotes_url;
 
     // empty constructor for Parceler
     public Episode() {
     }
 
-    public Episode(String title, String audioUrl, int audio_length, String id, String description, long date) {
+    public Episode(String title, String audioUrl, int audio_length, String id, String description, long date, String listennotes_url) {
         this.title = title;
         this.audioUrl = audioUrl;
         this.audio_length = audio_length;
         this.id = id;
         this.description = description;
         this.date = date;
+        this.listennotes_url = listennotes_url;
     }
 
 }
