2c37ed8a2fe153ceb0519d621fde18de7ce5b229
Joel Camargo <joelc1225@gmail.com>
2018-08-20 18:35:47 -0400

    Add String Utils class with static method to strip HTML tags from json response

--- app/src/main/java/com/joelcamargojr/androidhub/Utils/StringUtils.java
+++ app/src/main/java/com/joelcamargojr/androidhub/Utils/StringUtils.java
@@ -6,6 +6,12 @@ public class StringUtils {
 
     // Strips HTML tags from Strings received from API
     public static String stripHtml(String htmlString) {
+
+        // returns with all HTML tags
+//        return htmlString;
+
+        // returns cleaned up and presentable string
         return Html.fromHtml(htmlString).toString();
+
     }
 }
