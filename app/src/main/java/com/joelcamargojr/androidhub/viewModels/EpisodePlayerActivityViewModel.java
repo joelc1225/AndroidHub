647f28cb6841f53516ffd8bb89e58dc09d11edfc
Joel Camargo <joelc1225@gmail.com>
2018-11-12 22:54:52 -0500

    Add base ViewModel classes

--- app/src/main/java/com/joelcamargojr/androidhub/viewModels/EpisodePlayerActivityViewModel.java
+++ app/src/main/java/com/joelcamargojr/androidhub/viewModels/EpisodePlayerActivityViewModel.java
@@ -2,5 +2,22 @@ package com.joelcamargojr.androidhub.viewModels;
 
 import android.arch.lifecycle.ViewModel;
 
+import com.joelcamargojr.androidhub.model.Episode;
+
 public class EpisodePlayerActivityViewModel extends ViewModel {
+
+    // Weather forecast the user is looking at
+    private Episode mEpisode;
+
+    public EpisodePlayerActivityViewModel() {
+
+    }
+
+    public Episode getEpisode() {
+        return mEpisode;
+    }
+
+    public void setEpisode(Episode episode) {
+        mEpisode = episode;
+    }
 }
--- app/src/main/java/com/joelcamargojr/androidhub/viewModels/MainActivityViewModel.java
+++ app/src/main/java/com/joelcamargojr/androidhub/viewModels/MainActivityViewModel.java
@@ -2,5 +2,23 @@ package com.joelcamargojr.androidhub.viewModels;
 
 import android.arch.lifecycle.ViewModel;
 
+import com.joelcamargojr.androidhub.model.Episode;
+
+import java.util.List;
+
 public class MainActivityViewModel extends ViewModel {
+
+    // Weather forecast the user is looking at
+    private List<Episode> mEpisodes;
+
+    public MainActivityViewModel() {
+    }
+
+    public List<Episode> getEpisodes() {
+        return mEpisodes;
+    }
+
+    public void setEpisodeList(List<Episode> episodeList) {
+        mEpisodes = episodeList;
+    }
 }
