01eb6cc10a311d652687a0d1f25207b690f78239
Joel Camargo <joelc1225@gmail.com>
2018-11-27 18:53:12 -0500

    Update gradle and migrate build files\layout files to AndroidX

--- app/src/main/java/com/joelcamargojr/androidhub/Utils/InjectorUtils.java
+++ app/src/main/java/com/joelcamargojr/androidhub/Utils/InjectorUtils.java
@@ -11,14 +11,23 @@ public class InjectorUtils {
     /**
      *
      * Provides static methods to inject various classes needed for app
+     * one instantiation per app run through
      *
      */
 
+    public static Repository mRepository;
+    NetworkDatasource mNetworkDatasource;
+
     public static Repository provideRepository(Context context) {
-        EpisodeDatabase episodeDatabase = EpisodeDatabase.getInstance(context.getApplicationContext());
-        NetworkDatasource networkDatasource =
-                NetworkDatasource.getInstance(context.getApplicationContext());
-        return Repository.getInstance(episodeDatabase.episodeDao(), networkDatasource);
+
+        if (mRepository == null) {
+            EpisodeDatabase episodeDatabase = EpisodeDatabase.getInstance(context.getApplicationContext());
+            NetworkDatasource networkDatasource =
+                    provideNetworkDatasource(context.getApplicationContext());
+            return Repository.getInstance(episodeDatabase.episodeDao(), networkDatasource);
+        }
+        return mRepository;
+
     }
 
     public static NetworkDatasource provideNetworkDatasource(Context context) {
---
+++ app/src/main/java/com/joelcamargojr/androidhub/viewModels/MainViewModelFactory.java
@@ -0,0 +1,4 @@
+package com.joelcamargojr.androidhub.viewModels;
+
+public class MainViewModelFactory {
+}
